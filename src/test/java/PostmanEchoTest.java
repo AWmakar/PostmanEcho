2599d9fdcfe9279a16b504da49ea4580c3977b17
Andrey Makarov <makarov-andrew@inbox.ru>
2023-05-10 08:24:25 +0400

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,30 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPostmanEcho() {
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType(ContentType.JSON)
+                .body("Hello Netology") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body()
+        ;
+
+    }
+
+
+}
