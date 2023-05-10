4ef9e8cbd0e84e9592b81100b0711e94c95d96ae
Andrey Makarov <makarov-andrew@inbox.ru>
2023-05-10 08:53:31 +0400

    Add body

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -21,7 +21,7 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body()
+                .body("data", equalTo("Hello Netology"))
         ;
 
     }
